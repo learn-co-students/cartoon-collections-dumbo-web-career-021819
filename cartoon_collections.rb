200ddc3083508057827490f8ed50265d1af6d6e0
Jo K <joel.kohn42@gmail.com>
2019-01-14 06:11:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(person)
+  person.each_with_index do |person, index|
+    puts "#{index + 1} #{person}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map! do |item|
+    item = item.capitalize
+    item += "!"
+  end 
+  return arr 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.each do |word|  
+    item = word.to_s 
+    if item.length > 4
+      return true 
+    end 
+  end 
+    return false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(yum)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  yum.each do |hope|
+    if cheese_types.include?(hope)
+      return hope 
+    end 
+  end 
+  return nil 
 end
