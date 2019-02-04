93a3e5f551d293da4f6f6fdd94699b4e275100cd
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 15:12:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  puts dwarves.each_with_index {|d, i| i & d}# Your code here
 end
 
 def summon_captain_planet# code an argument here
