45860ad8441098414c3805e8534cebe835f032ed
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 15:56:55 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,13 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.reverse
-  array.each {|a| a.capitalize! << '!'}
+  #array.each {|a| a.capitalize! << '!'}
+  #better - doesn't change original array
+  array.map {|a| a.capitalize << "!"}
 end
 
-def long_planeteer_calls(array)
+# original
+def long_planeteer_calls1(array)
   array.each do |element|
     if element.length > 4
       return true
@@ -16,7 +18,12 @@ def long_planeteer_calls(array)
   return false
 end
 
+# faster
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4 }
+end
 
+#original
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
   foods.each do |food|
@@ -24,5 +31,11 @@ def find_the_cheese(foods)
       return food
     end
   end
-  nil 
+  nil
+end
+
+#alt
+def find_the_cheese_alt(foods)
+  cheeses = %w[gouda cheddar camembert]
+  foods.find {|a_food| cheeses.include?(a_food)}
 end
